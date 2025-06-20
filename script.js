e07c5213bd7f7331891906f14bacc0017c231f6d
anoor1 <averinoor2020@gmail.com>
2025-06-20 08:50:04 -0400

    html

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // JavaScript goes here.
+let mango=0
+let passionFruit=0
+let lychee=0 
+let dragonfruit=0
+
+// Get all image elements inside buttons
+const buttonImages = document.querySelectorAll("button img");
+
+// Loop through them and set new size
+buttonImages.forEach(img => {
+  img.style.width = Max;
+  img.style.height = Auto;
+});
+
+Q1A1=document.querySelector(".")
+
+/* document.querySelector(".result").style.display = "block"; */ 
+
